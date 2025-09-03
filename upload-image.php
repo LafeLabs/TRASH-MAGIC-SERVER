1c1c1724825ff4441fcc1220968b52260524e5d9
Trash Robot <lafelabs@gmail.com>
2025-09-03 05:52:27 -0600

    fixed upload-image.php links

--- upload-image.php
+++ upload-image.php
@@ -1,5 +1,5 @@
 <p style = "font-size:5em">
-    <a href = "freebox.html">FREEBOX</a>
+    <a href = "image-feed.html">IMAGE FEED</a>
 </p>
 
 <p style = "font-size:5em">
@@ -7,12 +7,11 @@
 </p>
 
 <?php
-$target_dir = "freestore/";
+
 $files = scandir(getcwd());
 $imageIndex =  count($files) - 1;
 $infilename = basename( $_FILES["fileToUpload"]["name"]);
 $extension = substr($infilename,-4);
-//$target_file = $target_dir . "image" . $imageIndex . $extension;
 $target_file = $infilename;
 $uploadOk = 1;
 
@@ -20,7 +19,6 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if file already exists
 if (file_exists($target_file)) {
     $imageIndex +=  1;
-   // $target_file = $target_dir . "image" . $imageIndex . $extension;
 }
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
